9890f2de95950b8da526aa55f24fa220c9f144b9
nekomona <nekomona@163.com>
2024-05-19 22:30:22 +0800

    Add custom HID IF

--- lib/CherryUSB/demo/cdc_acm_template.c
+++ lib/CherryUSB/demo/cdc_acm_template.c
@@ -1,18 +1,27 @@
 #include "usbd_core.h"
 #include "usbd_cdc.h"
+#include "usbd_hid.h"
 
 /*!< endpoint address */
 #define CDC_IN_EP  0x81
 #define CDC_OUT_EP 0x02
 #define CDC_INT_EP 0x83
 
-#define USBD_VID           0xFFFF
-#define USBD_PID           0xFFFF
+/*!< hidraw in endpoint */
+#define HIDRAW_IN_EP       0x86
+#define HIDRAW_IN_SIZE     64
+#define HIDRAW_IN_INTERVAL 0
+
+/*!< custom hid report descriptor size */
+#define HID_CUSTOM_REPORT_DESC_SIZE 16
+
+#define USBD_VID           0x2FE3
+#define USBD_PID           0x5652
 #define USBD_MAX_POWER     100
 #define USBD_LANGID_STRING 1033
 
 /*!< config descriptor size */
-#define USB_CONFIG_SIZE (9 + CDC_ACM_DESCRIPTOR_LEN)
+#define USB_CONFIG_SIZE (9 + CDC_ACM_DESCRIPTOR_LEN + 9 + 9 + 7)
 
 #ifdef CONFIG_USB_HS
 #define CDC_MAX_MPS 512
@@ -25,6 +34,33 @@ static const uint8_t cdc_descriptor[] = {
     USB_DEVICE_DESCRIPTOR_INIT(USB_2_0, 0xEF, 0x02, 0x01, USBD_VID, USBD_PID, 0x0100, 0x01),
     USB_CONFIG_DESCRIPTOR_INIT(USB_CONFIG_SIZE, 0x02, 0x01, USB_CONFIG_BUS_POWERED, USBD_MAX_POWER),
     CDC_ACM_DESCRIPTOR_INIT(0x00, CDC_INT_EP, CDC_OUT_EP, CDC_IN_EP, CDC_MAX_MPS, 0x02),
+    /************** Descriptor of Custom interface *****************/
+    0x09,                          /* bLength: Interface Descriptor size */
+    USB_DESCRIPTOR_TYPE_INTERFACE, /* bDescriptorType: Interface descriptor type */
+    0x00,                          /* bInterfaceNumber: Number of Interface */
+    0x00,                          /* bAlternateSetting: Alternate setting */
+    0x02,                          /* bNumEndpoints */
+    0x03,                          /* bInterfaceClass: HID */
+    0x01,                          /* bInterfaceSubClass : 1=BOOT, 0=no boot */
+    0x00,                          /* nInterfaceProtocol : 0=none, 1=keyboard, 2=mouse */
+    0,                             /* iInterface: Index of string descriptor */
+    /******************** Descriptor of Custom HID ********************/
+    0x09,                    /* bLength: HID Descriptor size */
+    HID_DESCRIPTOR_TYPE_HID, /* bDescriptorType: HID */
+    0x11,                    /* bcdHID: HID Class Spec release number */
+    0x01,
+    0x00,                        /* bCountryCode: Hardware target country */
+    0x01,                        /* bNumDescriptors: Number of HID class descriptors to follow */
+    0x22,                        /* bDescriptorType */
+    HID_CUSTOM_REPORT_DESC_SIZE, /* wItemLength: Total length of Report descriptor */
+    0x00,
+    /******************** Descriptor of Custom in endpoint ********************/
+    0x07,                         /* bLength: Endpoint Descriptor size */
+    USB_DESCRIPTOR_TYPE_ENDPOINT, /* bDescriptorType: */
+    HIDRAW_IN_EP,                 /* bEndpointAddress: Endpoint Address (IN) */
+    0x03,                         /* bmAttributes: Interrupt endpoint */
+    WBVAL(HIDRAW_IN_SIZE),        /* wMaxPacketSize: 64 Byte max */
+    HIDRAW_IN_INTERVAL,           /* bInterval: Polling Interval */
     ///////////////////////////////////////
     /// string0 descriptor
     ///////////////////////////////////////
@@ -98,6 +134,53 @@ static const uint8_t cdc_descriptor[] = {
 #endif
     0x00
 };
+/*!< custom hid report descriptor */
+static const uint8_t hid_custom_report_desc[HID_CUSTOM_REPORT_DESC_SIZE] = {
+    /* USER CODE BEGIN 0 */
+	0x06, 0x00, 0xFF, // USAGE_PAGE (Vendor Defined Page 1)
+	0x09, 0x00,       // USAGE (Undefined)
+	0xa1, 0x01,       // COLLECTION (Application)
+	0x09, 0x00,       //   USAGE (Vendor Usage 1)
+	0x75, 0x08,       // REPORT_SIZE (8)
+	0x95, 0x3C,       // REPORT_COUNT (60)
+	0x81, 0x02,       // INPUT (Data,Var,Abs)
+
+	0xc0 // END_COLLECTION
+};
+
+USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t read_buffer[64];
+USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t write_buffer[64];
+
+#define HID_STATE_IDLE 0
+#define HID_STATE_BUSY 1
+
+/*!< hid state ! Data can be sent only when state is idle  */
+static volatile uint8_t custom_state = HID_STATE_IDLE;
+
+static void usbd_hid_custom_in_callback(uint8_t ep, uint32_t nbytes)
+{
+    USB_LOG_RAW("actual in len:%d\r\n", nbytes);
+    custom_state = HID_STATE_IDLE;
+}
+
+static struct usbd_endpoint custom_in_ep = {
+    .ep_cb = usbd_hid_custom_in_callback,
+    .ep_addr = HIDRAW_IN_EP
+};
+
+void hid_custom_test(void)
+{
+    const uint8_t sendbuffer[8] = { 0x00, 0x00, HID_KBD_USAGE_A, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+    memcpy(write_buffer, sendbuffer, 8);
+    int ret = usbd_ep_start_write(HIDRAW_IN_EP, write_buffer, 8);
+    if (ret < 0) {
+        return;
+    }
+    custom_state = HID_STATE_BUSY;
+    while (custom_state == HID_STATE_BUSY) {
+    }
+}
 
 USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t read_buffer[2048];
 USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t write_buffer[2048];
@@ -173,6 +256,7 @@ struct usbd_endpoint cdc_in_ep = {
 
 struct usbd_interface intf0;
 struct usbd_interface intf1;
+struct usbd_interface intf2;
 
 void cdc_acm_init(void)
 {
@@ -186,28 +270,11 @@ void cdc_acm_init(void)
     usbd_add_interface(usbd_cdc_acm_init_intf(&intf1));
     usbd_add_endpoint(&cdc_out_ep);
     usbd_add_endpoint(&cdc_in_ep);
+
+    usbd_add_interface(usbd_hid_init_intf(&intf2, hid_custom_report_desc, HID_CUSTOM_REPORT_DESC_SIZE));
+    usbd_add_endpoint(&custom_in_ep);
+
     usbd_initialize();
-}
-
-volatile uint8_t dtr_enable = 0;
-
-void usbd_cdc_acm_set_dtr(uint8_t intf, bool dtr)
-{
-    if (dtr) {
-        dtr_enable = 1;
-    } else {
-        dtr_enable = 0;
-    }
-}
-
-void cdc_acm_data_send_with_dtr_test(void)
-{
-    if (dtr_enable) {
-        ep_tx_busy_flag = true;
-        usbd_ep_start_write(CDC_IN_EP, write_buffer, 2048);
-        while (ep_tx_busy_flag) {
-        }
-    }
 }
 
 int _write(int fd, char *buf, int size)
