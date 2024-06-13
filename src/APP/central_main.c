d484cba29acdb467ebf2a3986cc082caa0f12dd3
nekomona <nekomona@163.com>
2024-06-13 18:56:16 +0800

    Boost clock speed

--- src/APP/central_main.c
+++ src/APP/central_main.c
@@ -55,7 +55,7 @@ int main(void)
 #if(defined(DCDC_ENABLE)) && (DCDC_ENABLE == TRUE)
     PWR_DCDCCfg(ENABLE);
 #endif
-    SetSysClock(CLK_SOURCE_PLL_60MHz);
+    SetSysClock(CLK_SOURCE_PLL_80MHz);
 #if(defined(HAL_SLEEP)) && (HAL_SLEEP == TRUE)
     GPIOA_ModeCfg(GPIO_Pin_All, GPIO_ModeIN_PU);
     GPIOB_ModeCfg(GPIO_Pin_All, GPIO_ModeIN_PU);
